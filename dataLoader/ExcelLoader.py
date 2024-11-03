a397e2e8e50cbba5bbb3d6d5156e9981eace9061
Gvln <santis.paez2414@outlook.com>
2024-11-03 02:47:20 -0500

    feat: solution id

--- dataLoader/ExcelLoader.py
+++ dataLoader/ExcelLoader.py
@@ -9,17 +9,18 @@ def ExcelData():
 
     studyColum = 3
 
-    for row in range(1, 10):
+    for row in range(1, 50):
         studyArray = [ unidecode(word).lower() for word in data.iloc[row, studyColum].split() ]
         print(data.iloc[row, 0], end=' | ')
 
         # index = index of each word, studyWord = all words in the array
+        nodule = Nodule("Null", "Null")
         for index, studyWord in enumerate(studyArray):
+
             if Nodule.noduleStateList[0] in studyWord:
                 before = studyArray[max(0, index - 3):index]
                 after = studyArray[index + 1:min(len(studyArray), index + 4)]
 
-                nodule = Nodule("Null", "Null")
 #               beforeText = " ".join(before)
                 if 'no' in before:
                     nodule = Nodule("No", "Null")
@@ -30,7 +31,7 @@ def ExcelData():
                             nodule = Nodule("Yes", noduleMorphology) 
                         else:
                             nodule = Nodule("Yes", "Null") 
-                print(nodule)
+        print(nodule)
 
 
 
