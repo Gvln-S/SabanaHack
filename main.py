839592ee7162bafbb1deb8ad6ba9ceca1759fd78
Oscarvm117 <oscarvemo@unisabana.edu.co>
2024-11-02 21:32:08 -0500

    created class findings

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from dataLoader.excel_loader import excelData
+from wordBank.findings import findings
 
+
+findings()
 excelData()
+palabra = "nodulo"
+palabra2= "si"
+
+findings_instance=findings()
+if palabra in findings_instance.findings_list and palabra2 in findings_instance.findings_yesorno:
+    print("si existe esta palbra")
+else:
+    print("no se encuentra esta palabra ")
+
+
